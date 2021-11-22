430806c31d5633ee0ed0af7ead2e8c2606699953
kevinandreass <kevinandreas@mhs.mdp.ac.id>
2021-11-22 22:25:35 +0700

    ver 0.2.4.3x
    - trying to fix :")

--- src/views/login/main.vue
+++ src/views/login/main.vue
@@ -1,29 +1,26 @@
 <template>
-    <div>
-        <h1>Login</h1>
-        <main class="form-signin">
-            <form>
-                <img class="mb-4" src="@/assets/icon.png" alt="">
-                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
-                <div class="form-floating">
+    <div class="text-center">
+    <main class="form-signin">
+        <form>
+            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+            <div class="form-floating">
                 <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
                 <label for="floatingInput">Email address</label>
-                </div>
-                <div class="form-floating">
+            </div>
+            <div class="form-floating">
                 <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
                 <label for="floatingPassword">Password</label>
-                </div>
+            </div>
 
-                <div class="checkbox mb-3">
+            <div class="checkbox mb-3">
                 <label>
                     <input type="checkbox" value="remember-me"> Remember me
                 </label>
-                </div>
-                <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-                <p class="mt-5 mb-3 text-muted">© 2017–2021</p>
-            </form>
-        </main>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+            <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+        </form>
+    </main>
     </div>
 </template>
 
@@ -32,3 +29,28 @@ export default {
     name: "Login"
 }
 </script>
+
+<style scoped>
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
--- src/views/register/main.vue
+++ src/views/register/main.vue
@@ -1,6 +1,45 @@
 <template>
-    <div>
-        <h1>Register</h1>
+    <div class="container pt-5">
+        <h1 class="text-center h3 mb-3 fw-normal">REGISTER</h1>
+        <form>
+            <div class="mb-3">
+                <label for="" class="form-label">Nama</label>
+                <input type="text" class="form-control" id="" aria-describedby="">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">HP</label>
+                <input type="text" class="form-control" id="" aria-describedby="">
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Alamat</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <div class="mb-3">
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="jk" id="rbL">
+                    <label class="form-check-label" for="rbL">
+                        Laki-laki
+                    </label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="jk" id="rbP" checked>
+                    <label class="form-check-label" for="rbP">
+                        Perempuan
+                    </label>
+                </div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            
+            <button type="submit" class="btn btn-success me-1">Register</button>
+            <button type="reset" class="btn btn-danger">Reset</button>
+        </form>
     </div>
 </template>
 
@@ -9,3 +48,28 @@ export default {
     name: "Register"
 }
 </script>
+
+<style scoped>
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
