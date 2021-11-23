9c9fd6dc9de2ac06005331e6786c716198bf2aec
kevinandreass <kevinandreas@mhs.mdp.ac.id>
2021-11-23 19:18:30 +0700

    ver 0.2.4.7x
    - last fix?

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "./kevinandreas-vue/" : "./",
+    publicPath: process.env.NODE_ENV === "production" ? "/kevinandreas-vue/" : "/",
   };
